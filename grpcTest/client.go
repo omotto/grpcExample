e76e2d95122d977beb2bfe5d20bdbb17b9c6ab02
omotto <oscar.motto@forcemanager.net>
2019-10-03 09:41:25 +0200

    added interceptor for auth token

--- grpcTest/client.go
+++ grpcTest/client.go
@@ -2,16 +2,52 @@ package main
 
 import (
 	"context"
+	"golang.org/x/oauth2"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/oauth"
+	"google.golang.org/grpc/testdata"
 	"grpcExample/pb"
 	"log"
 	"time"
 )
 
-const address = "localhost:7777"
+const (
+	address = "localhost:7777"
+	fallbackToken = "some-secret-token"
+)
+
+// unaryInterceptor in order to add auth for each sent message
+func clientUnaryInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	var credsConfigured bool
+	for _, o := range opts {
+		_, ok := o.(grpc.PerRPCCredsCallOption)
+		if ok {
+			credsConfigured = true
+			break
+		}
+	}
+	if !credsConfigured {
+		opts = append(opts, grpc.PerRPCCredentials(oauth.NewOauthAccess(&oauth2.Token{
+			AccessToken: fallbackToken,
+		})))
+	}
+	start := time.Now()
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	end := time.Now()
+	log.Printf("RPC: %s, start time: %s, end time: %s, err: %v", method, start.Format("Basic"), end.Format(time.RFC3339), err)
+	return err
+}
+
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	// Create tls based credential.
+	creds, err := credentials.NewClientTLSFromFile(testdata.Path("ca.pem"), "x.test.youtube.com")
+	if err != nil {
+		log.Fatalf("failed to load credentials: %v", err)
+	}
+
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(creds)/*, grpc.WithInsecure()*/, grpc.WithUnaryInterceptor(clientUnaryInterceptor))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
--- grpcTest/server.go
+++ grpcTest/server.go
@@ -3,11 +3,16 @@ package main
 import (
 	"context"
 	"crypto/md5"
+	"errors"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/testdata"
 	"grpcExample/pb"
 	"log"
 	"net"
+	"strings"
 )
 
 const port = ":7777"
@@ -34,15 +39,49 @@ func (s *exampleService) Generate(ctx context.Context, in *pb.ExampleRequest) (*
 	return &pb.ExampleResponse{Url: MyMethod(in.Email, in.Size)}, nil
 }
 
+func valid(authorization []string) bool {
+	if len(authorization) < 1 {
+		return false
+	}
+	token := strings.TrimPrefix(authorization[0], "Bearer ")
+	// Perform the token validation here. For the sake of this example, the code
+	// here forgoes any of the usual OAuth2 token validation and instead checks
+	// for a token matching an arbitrary string.
+	return token == "some-secret-token"
+}
+
+func serverUnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	// authentication (token verification)
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("errMissingMetadata")
+	}
+	if !valid(md["authorization"]) {
+		return nil, errors.New("errInvalidToken")
+	}
+	m, err := handler(ctx, req)
+	if err != nil {
+		log.Printf("RPC failed with error %v", err)
+	}
+	return m, err
+}
+
 // main start a gRPC server and waits for connection
 func main() {
+
+	// Create tls based credential.
+	creds, err := credentials.NewServerTLSFromFile(testdata.Path("server1.pem"), testdata.Path("server1.key"))
+	if err != nil {
+		log.Fatalf("failed to create credentials: %v", err)
+	}
+
 	// create a listener on TCP port 7777
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("Failed to listen on port: %v", err)
 	}
 	// create a gRPC server object
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(creds), grpc.UnaryInterceptor(serverUnaryInterceptor))
 
 	pb.RegisterExmapleServiceServer(grpcServer, &exampleService{})
 
